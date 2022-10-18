090845053b97760de3bfc6fe4743cfd82b90211f
mtarasenko <wick71@yandex.ru>
2022-10-18 09:40:41 +0300

    add STATIC_ROOT

--- settings/settings.py
+++ settings/settings.py
@@ -100,6 +100,9 @@ USE_I18N = True
 
 USE_TZ = True
 
-STATIC_URL = 'static/'
+STATIC_URL = "/static/"
+MEDIA_URL = "/media/"
+STATIC_ROOT = BASE_DIR / "static"
+MEDIA_ROOT = BASE_DIR / "media"
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
